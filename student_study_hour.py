4dba5b4bc2b9176db002da57c8cf10f5085411ad
ayushi <ayushi.singh@gammaedge.io>
2025-11-11 11:35:11 +0530

    print

--- student_study_hour.py
+++ student_study_hour.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+print("Student Study Hour vs Exam Score Prediction using Linear Regression\n")
 
 
 X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=float)
